f7a7b9df738bfeba49780338fca9fc66f1c8b3d7
jcbird <jonathan.bird.io@gmail.com>
2020-09-23 17:15:00 -0500

    main classes now avaiable through main module

--- src/ppv/config.py
+++ src/ppv/config.py
@@ -4,19 +4,16 @@ Parameter configuration
 from configparser import ConfigParser
 from pathlib import Path
 
-# Fixed relative path
+## Fixed relative path
 _plansummary = Path(__file__).parent / 'data/platePlans_sdss5.fits'
+##
 
-
+## Configuration
 config = ConfigParser()
 config.read('setup.ini')
 
-
 plate_dir = Path(config['paths']['plate_dir'])
+## end Configuration
 
-
-# To be changed by user
-##  plate_dir = Path.home() / 'obsdata/plates'
-# five_dir = Path.home() / 'obsdata/plates'
 
 
