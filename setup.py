81cf94f9da9058d9d476db4842f3c8304c3e54b2
jcbird <jonathan.bird.io@gmail.com>
2020-09-23 15:33:43 -0500

    cleanup

--- setup.py
+++ setup.py
@@ -7,15 +7,5 @@ setup(
     version='0.0.1',
     description='Tools for dealing with SDSS-V plate files and plate runs.',
     author='Jonathan Bird',
+    url='https://github.com/jcbird/ppv',
     license='BSD-3')
-
-# setup(name='sdss5ops',
-#       version='0.0.1',
-#       description='SDSS-V OPS tools',
-#       url='http://github.com/jcbird/ops',
-#       author='Jonathan Bird',
-#       author_email='flyingcircus@example.com',
-#       license='MIT',
-#       packages=find_packages(where='src'),
-#       package_dir={"": "src"},
-#       zip_safe=False)
