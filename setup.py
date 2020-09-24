3020433a85a897db5e3664294b1c144596926121
jcbird <jonathan.bird.io@gmail.com>
2020-09-24 16:45:33 -0500

    add package data fits file

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='ppv',
     packages=find_packages(where='src'),
     package_dir={"": "src"},
+    package_data={"": ["data/platePlans_sdss5.fits"]},
     version='0.0.1',
     description='Tools for dealing with SDSS-V plate files and plate runs.',
     author='Jonathan Bird',
