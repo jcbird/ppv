42ef7d6d4bb435c2ca2cd620a19946b7b30b5c52
jcbird <jonathan.bird.io@gmail.com>
2020-09-25 14:03:24 -0500

    no autoloading of classes, easier debugging

--- src/ppv/__init__.py
+++ src/ppv/__init__.py
@@ -1,17 +1,8 @@
 from .data import io as data_io
 
-
 allplate_summary = data_io.load_plansummary()
 allplate_summary.add_index('name')  # for quick filtering on fieldname
 _names_array = allplate_summary['name'].astype('U')  # for quick checking
 _platerun_array = allplate_summary['platerun'].astype('U')  # for quick checking
 
 available_plateruns = list(set(_platerun_array))
-
-try:
-    from .plate import Plate
-    from .targets import Targets
-    from .groups import Field, Platerun
-except (AttributeError, ImportError) :  # Configuration file was missing / testing
-    pass
-
