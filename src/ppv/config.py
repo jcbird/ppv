1a94dfa8c395716e7a91c056086618a7453b7b11
jcbird <jonathan.bird.io@gmail.com>
2020-10-02 15:29:42 -0500

    config parser now sees fiveplates_dir

--- src/ppv/config.py
+++ src/ppv/config.py
@@ -13,7 +13,8 @@ config = ConfigParser()
 
 try:
     config.read_file(open(os.fspath(_config_path), 'r'))
-    plate_dir = Path(config['paths']['plate_dir'])
+    plate_dir = Path(config.get('paths', 'plate_dir', fallback=None))
+    fiveplates_dir = Path(config.get('paths', 'fiveplates_dir', fallback=Path.home()))
     utah_username = config['username']['sdss_org']
 except FileNotFoundError:
     print(f'Configuration file does not exist!')
@@ -21,6 +22,5 @@ except FileNotFoundError:
     print(f'AND edit the configuration file before proceeding')
 
 ## end Configuration
-
 
 
