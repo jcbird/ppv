8cf19f357fb0c178b8dacbbda7d537ae374cce34
jcbird <jonathan.bird.io@gmail.com>
2020-10-02 15:55:08 -0500

    paths added for five_plates repo

--- src/ppv/util/paths.py
+++ src/ppv/util/paths.py
@@ -78,3 +78,63 @@ def plateholes(platenum):
     """
     filename = plateholes_file(platenum)
     return plate(platenum) / filename
+
+def fiveplates_platerun(platerun):
+    """
+    gets directory of platerun in five_plates repo.
+
+    Parameters
+    ----------
+    platerun : str
+        identifier of platerun, e.g. '2020.08.x.mwm-bhm'
+    
+    """
+    return config.fiveplates_dir / platerun
+
+def fiveplates_summary(platerun):
+    """
+    path to summary file in five_plates repo.
+
+    Parameters
+    ----------
+    platerun : str
+        identifier of platerun, e.g. '2020.08.x.mwm-bhm'
+    """
+    summary_file = f'{platerun}.summary'
+    return fiveplates_platerun(platerun) / summary_file
+
+def fiveplates_fieldfiles(platerun):
+    """
+    path to zip file containing fields_files in five_plates repo.
+
+    Parameters
+    ----------
+    platerun : str
+        identifier of platerun, e.g. '2020.08.x.mwm-bhm'
+    """
+    field_files = f'{platerun}_field_files.zip'
+    return fiveplates_platerun(platerun) / field_files
+
+def fiveplates_clean_field_file(field):
+    """
+    string representation of targets_clean file for field within
+    fiveplates_field_files zip file.
+
+    Parameters
+    ----------
+    field : str
+        identifier of field, e.g. 'GG_010'
+    """
+    return f'{field}_targets_clean.txt'
+
+def fiveplates_field_file(field):
+    """
+    string representation of targets.txt file for field within
+    fiveplates_field_files zip file.
+
+    Parameters
+    ----------
+    field : str
+        identifier of field, e.g. 'GG_010'
+    """
+    return f'{field}_targets.txt'
