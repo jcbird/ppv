7de33d99b3da4b2bb4a247f44ed5e79c0603e8e8
jcbird <jonathan.bird.io@gmail.com>
2020-11-23 09:55:41 -0600

    updated version to 0.4a

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='ppv',
     packages=find_packages(where='src'),
     package_dir={"": "src"},
-    version='0.36',
+    version='0.4a',
     description='Tools for dealing with SDSS-V plate files and plate runs.',
     author='Jonathan Bird',
     url='https://github.com/jcbird/ppv',
